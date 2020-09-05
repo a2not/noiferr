36390f14945f9787c0e700b1b03f2cbdd4a6be90
Khdbble <fb503d52.af1c4d@gmail.com>
2020-09-05 19:43:42 +0900

    dev: implement noiferr

--- noiferr.go
+++ noiferr.go
@@ -1,11 +1,12 @@
 package noiferr
 
 import (
-	"go/ast"
+	"go/token"
+	"go/types"
 
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/ast/inspector"
+	"golang.org/x/tools/go/analysis/passes/buildssa"
+	"golang.org/x/tools/go/ssa"
 )
 
 const doc = "noiferr is ..."
@@ -16,26 +17,69 @@ var Analyzer = &analysis.Analyzer{
 	Doc:  doc,
 	Run:  run,
 	Requires: []*analysis.Analyzer{
-		inspect.Analyzer,
+		buildssa.Analyzer,
 	},
 }
 
+var errType = types.Universe.Lookup("error").Type().Underlying().(*types.Interface)
+
 func run(pass *analysis.Pass) (interface{}, error) {
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-
-	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
-	}
-
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+	funcs := pass.ResultOf[buildssa.Analyzer].(*buildssa.SSA).SrcFuncs
+	for _, f := range funcs {
+		for _, b := range f.Blocks {
+			for _, instr := range b.Instrs {
+				switch instr := instr.(type) {
+				case *ssa.Call:
+					if !isHandledWithCond(instr) {
+						pass.Reportf(instr.Pos(), "error received but not handled")
+					}
+				}
 			}
 		}
-	})
+	}
 
 	return nil, nil
 }
 
+func isError(typ types.Type) bool {
+	return types.Implements(typ, errType) || types.Implements(types.NewPointer(typ), errType)
+}
+
+func isHandledWithCond(callInstr *ssa.Call) bool {
+	switch typ := callInstr.Type().(type) {
+	case *types.Tuple:
+		for i := 0; i < typ.Len(); i++ {
+			if isError(typ.At(i).Type()) {
+				ref := (*callInstr.Referrers())[i]
+				extract, ok := ref.(*ssa.Extract)
+				if ok && !errorVarHandled(extract.Referrers()) {
+					return false
+				}
+			}
+		}
+	default:
+		if isError(typ) {
+			return errorVarHandled(callInstr.Referrers())
+		}
+	}
+	return true
+}
+
+// func errorVarHandeld(binop *ssa.BinOp) bool {
+func errorVarHandled(instrs *[]ssa.Instruction) bool {
+	for _, instr := range *instrs {
+		switch instr := instr.(type) {
+		case *ssa.BinOp:
+			if instr.Op != token.NEQ && instr.Op != token.EQL {
+				continue
+			}
+			for _, ref := range *instr.Referrers() {
+				_, ok := ref.(*ssa.If)
+				if ok {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
