1dd016ad59691083146f9e953d019040d1119733
Khdbble <fb503d52.af1c4d@gmail.com>
2020-09-05 19:42:46 +0900

    add test cases

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,8 +1,42 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+import "errors"
+
+func f() (int, error) {
+	return 0, nil
 }
 
+var (
+	_, err1 = f() // OK
+)
+
+func main() {
+	_, err := f() // OK
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := f(); err != nil { // OK
+		panic(err)
+	}
+
+	if _, err := f(); err == nil { // OK
+		panic(err)
+	}
+
+	_, err = f() // want "error received but not handled"
+
+	err = func() error {
+		return nil
+	}() // want "error received but not handled"
+
+	if a, err := f(); a != 0 { // want "error received but not handled"
+		panic(err)
+	}
+
+	_, err = f() // want "error received but not handled"
+
+	b := errors.New("foo") // want "error received but not handled"
+
+	print(b)
+}
